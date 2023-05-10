bccf4ecf9f546a95ed47d1b5acbe60b02a6759b2
Vinicius Roosevelt <catce.2021111tads0197@aluno.ifpi.edu.br>
2023-05-10 17:55:26 -0300

    Guard um if mas com else obrigatório

---
+++ guard.swift
@@ -0,0 +1,10 @@
+func comparaTimes(Time1: Int,Time2:Int){
+    guard Time1 > Time2 else {
+    print("Time2 é maior")
+    return 
+}
+    print("Time1 é Maior")
+}
+
+comparaTimes(Time1: 10,Time2:12)
+
